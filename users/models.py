66417ce3c00f5a7608254ac2b5fdd065a4d5253c
Shureed <siamriad.556@gmail.com>
2025-06-30 15:50:30 +0600

    Initial project commit with all Django files

---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image.name != 'default.jpeg':
+            img = Image.open(self.image.path)
+
+            # Crop to center square
+            width, height = img.size
+            min_dim = min(width, height)
+            left = (width - min_dim) / 2
+            top = (height - min_dim) / 2
+            right = (width + min_dim) / 2
+            bottom = (height + min_dim) / 2
+            img = img.crop((left, top, right, bottom))
+
+            # Resize to 300x300
+            output_size = (300, 300)
+            img.thumbnail(output_size, Image.Resampling.LANCZOS)  # Updated here
+
+            img.save(self.image.path)
